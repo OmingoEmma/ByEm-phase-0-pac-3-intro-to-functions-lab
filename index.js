cbe1fe7354a556b48abff5766de776e68e865dbf
Emma <emmaomingo@gmail.com>
2024-03-18 04:06:47 +0300

    JS LIBRARIES

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function sayHiToHeadphonedRoommate() {
+    const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+
+  }
+
+  
